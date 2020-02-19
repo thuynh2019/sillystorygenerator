e5a83e813439c91171fc82e0a145f4faa897082a
thuynh2019 <ty.huynh@smail.astate.edu>
2020-02-18 23:53:35 -0600

    final final commit

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300*0.0714286) + ' stone';
-    const temperature =  Math.round((94-32) * 5/9) + ' centigrade';
+    var weight = Math.round(300*0.0714286) + ' stone';
+    var temperature =  Math.round((94-32) * 5/9) + ' centigrade';
     newStory = newStory.replace('94 fahrenheit', temperature);
     newStory = newStory.replace('300 pounds', weight);
   }
